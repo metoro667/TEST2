b4a53c05816319073e36457776c25f1e3254b323
kanta <metoro667@gmail.com>
2016-05-17 13:59:00 +0900

    IOS用テスト

--- sample/ViewController.swift
+++ sample/ViewController.swift
@@ -10,9 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webview: UIWebView!
+    
+    var url:String = "https://www.google.co.jp"
+    
+    @IBAction func unipa(sender: AnyObject) {
+        
+        // 入力されたURLを取得
+        url = "https://portal.sa.dendai.ac.jp/up/faces/login/Com00505A.jsp"
+        
+        // loadURL関数を実行
+        loadURL()
+    }
+    @IBAction func mail(sender: AnyObject) {
+        
+        url = "http://mail.ms.dendai.ac.jp/cgi-bin/index.cgi"
+        
+        loadURL()
+    }
+    @IBAction func google(sender: AnyObject) {
+        
+        url = "https://www.google.co.jp"
+        
+        loadURL()
+    }
+    
+    // loadURL関数を定義
+    func loadURL() {
+        let requestURL = NSURL(string: url)
+        let request = NSURLRequest(URL: requestURL!)
+        webview.loadRequest(request)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        loadURL()
     }
 
     override func didReceiveMemoryWarning() {

e31bcb09a4b4de89df0b0ec94e08f3f9fa830d4c
kanta <metoro667@gmail.com>
2016-05-17 19:58:56 +0900

    unapt表示のボードを追加

--- 実習/WebView.swift
+++ 実習/WebView.swift
@@ -10,7 +10,53 @@ import Foundation
 import UIKit
 
 class WebView: UIViewController {
-    override func viewDidLoad() {
+    
+    @IBOutlet weak var webview: UIWebView!
+    
+    var url:String = "https://www.google.co.jp"
+    
+    @IBAction func unipa(sender: AnyObject) {
+        // 入力されたURLを取得
+        url = "https://portal.sa.dendai.ac.jp/up/faces/login/Com00505A.jsp"
         
+        // loadURL関数を実行
+        loadURL()
+    }
+    
+    @IBAction func google(sender: AnyObject) {
+        // 入力されたURLを取得
+        url = "https://www.google.co.jp"
+        
+        // loadURL関数を実行
+        loadURL()
+    }
+    
+    @IBAction func mail(sender: AnyObject) {
+        //入力されたURLを取得
+        url = "http://mail.ms.dendai.ac.jp/cgi-bin/index.cgi"
+        
+        // loadURL関数を実行
+        loadURL()
+    }
+    
+    
+    // loadURL関数を定義
+    func loadURL() {
+        let requestURL = NSURL(string: url)
+        let request = NSURLRequest(URL: requestURL!)
+        webview.loadRequest(request)
+    }
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        loadURL()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 }
